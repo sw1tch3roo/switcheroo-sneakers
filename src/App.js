3a9fe7a4dc1f0e0946e6cdd74f233809dea4364e
Дима Гусев <chi3fno1d@gmail.com>
2022-06-10 00:49:18 +0300

    Small Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,82 @@
 
 function App() {
   return (
-    <div className="wrapper">
+    <div className="wrapper clear">
+      <div className="mainShadow">
+        <div className="drawer d-flex flex-column">
+          <h2 className="d-flex justify-between mb-30">
+            Basket{" "}
+            <img
+              className="basketRemove cu-p" // cursor-pointer (macro-css)
+              width={30}
+              height={30}
+              src="/img/cancel.svg"
+              alt="Remove"
+            />
+          </h2>
+          <div className="items">
+            <div className="basketItem d-flex align-center mb-20">
+              {/* <img
+              className="mr-20"
+              width={125}
+              height={125}
+              src="img/sneakers/blazer.jpg"
+              alt="Sneakers"
+            /> */}
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/blazer.jpg)" }}
+                className="basketItemImg"
+              ></div>
+              <div className="flex">
+                <p className="mb-5">Men's shoes Nike Blazer Mid'077</p>
+                <b>12 999 rub.</b>
+              </div>
+              <img
+                className="basketRemove"
+                width={30}
+                height={30}
+                src="/img/cancel.svg"
+                alt="Remove"
+              />
+            </div>
+            <div className="basketItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/blazer.jpg)" }}
+                className="basketItemImg"
+              ></div>
+              <div className="flex">
+                <p className="mb-5">Men's shoes Nike Blazer Mid'077</p>
+                <b>12 999 rub.</b>
+              </div>
+              <img
+                className="basketRemove"
+                width={30}
+                height={30}
+                src="/img/cancel.svg"
+                alt="Remove"
+              />
+            </div>
+          </div>
+          <div className="basketTotalBlock">
+            <ul>
+              <li>
+                <span>Total:</span>
+                <div></div>
+                <b>21 498 rub.</b>
+              </li>
+              <li>
+                <span>Tax 5%:</span>
+                <div></div>
+                <b>1074 rub.</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Order <img src="/img/arrow.svg" alt="Arrow" />
+            </button>
+          </div>
+        </div>
+      </div>
+
       <header>
         {/* делаю левую часть шапки */}
         <div className="headerLeft">
@@ -24,12 +99,22 @@ function App() {
         </ul>
       </header>
       <div className="content">
-        <h1>All stories</h1>
+        {/* тут надпись 'все кроссовки' и поиск */}
+        <div className="d-flex align-center justify-between">
+          <h1>All shoes</h1>
+          <div className="search-block d-flex">
+            <img width={15} height={15} src="/img/search.png" alt="Search" />
+            <input placeholder="Search..." />
+          </div>
+        </div>
         <div className="sneakers">
           <div className="card">
+            <div className="favorite">
+              <img src="/img/heartUnliked.svg" alt="Unliked" />
+            </div>
             <img
-              width={170}
-              height={170}
+              width={150}
+              height={150}
               src="/img/sneakers/asics.jpeg"
               alt=""
             />
@@ -46,12 +131,12 @@ function App() {
           </div>
           <div className="card">
             <img
-              width={170}
-              height={170}
-              src="/img/sneakers/asics.jpeg"
+              width={150}
+              height={150}
+              src="/img/sneakers/blazer.jpg"
               alt=""
             />
-            <h5>Men's shoes Asics Gel Kayano</h5>
+            <h5>Men's shoes Nike Blazer Mid'077</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
                 <span>Price: </span>
@@ -64,12 +149,12 @@ function App() {
           </div>
           <div className="card">
             <img
-              width={170}
-              height={170}
-              src="/img/sneakers/asics.jpeg"
+              width={150}
+              height={150}
+              src="/img/sneakers/jordan.jpg"
               alt=""
             />
-            <h5>Men's shoes Asics Gel Kayano</h5>
+            <h5>Men's shoes Air Jordan 1 Mid</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
                 <span>Price: </span>
@@ -82,12 +167,12 @@ function App() {
           </div>
           <div className="card">
             <img
-              width={170}
-              height={170}
-              src="/img/sneakers/asics.jpeg"
+              width={150}
+              height={150}
+              src="/img/sneakers/dunk.jpeg"
               alt=""
             />
-            <h5>Men's shoes Asics Gel Kayano</h5>
+            <h5>Men's shoes Nike Dunk Low Scrap Sea Glass</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
                 <span>Price: </span>
